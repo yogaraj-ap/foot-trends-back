82a0753d11ea193b6a0b52c9da9805d499fa57f2
yogaraj-ap <yogaraj4542706@gmail.com>
2026-03-15 13:12:08 +0530

    m

--- src/main/java/com/trendfoots/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.trendfoots.repository;
-
-import com.trendfoots.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- src/main/java/com/trendfoots/repository/WishlistRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trendfoots.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.trendfoots.entity.Wishlist;
-
-public interface WishlistRepository extends JpaRepository<Wishlist,Long> {
-}
